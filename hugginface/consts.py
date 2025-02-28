645084c3af636b3462bbf647c07ea679aa2f7795
AlexFerrando <alexferrando15@gmail.com>
2025-02-28 13:19:52 +0100

    huggingface inference working

--- hugginface/consts.py
+++ hugginface/consts.py
@@ -1,1 +1,8 @@
-KITTI_MOTS_PATH = '/home/mcv/datasets/C5/KITTI-MOTS/'
+# Path to data
+KITTI_MOTS_PATH = '/home/mcv/datasets/C5/KITTI_MOTS/'
+KITTI_MOTS_PATH_ALEX = '/home/alex/Documents/MCV/C5/KITTI_MOTS/'
+
+# Model info
+MODEL_NAME = "facebook/detr-resnet-50"
+WIDTH_SIZE = 1242
+HEIGHT_SIZE = 375
--- hugginface/main.py
+++ hugginface/main.py
@@ -1,7 +1,39 @@
 import consts
+import torch
+from PIL import ImageDraw
+from read_data import read_data
+from transformers import AutoImageProcessor, AutoModelForObjectDetection
 
 def main():
-    print(consts.KITTI_MOTS_PATH)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    dataset = read_data(consts.KITTI_MOTS_PATH_ALEX)
+    image_processor = AutoImageProcessor.from_pretrained(consts.MODEL_NAME)
+    model = AutoModelForObjectDetection.from_pretrained(consts.MODEL_NAME)
+    model.to(device)
+    
+    image = dataset['test']['image'][0]
+    with torch.no_grad():
+        inputs = image_processor(images=[image], return_tensors="pt")
+        outputs = model(**inputs.to(device))
+        target_sizes = torch.tensor([[image.size[1], image.size[0]]])
+        results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
+
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            box = [round(i, 2) for i in box.tolist()]
+            print(
+                f"Detected {model.config.id2label[label.item()]} with confidence "
+                f"{round(score.item(), 3)} at location {box}"
+            )
+
+        draw = ImageDraw.Draw(image)
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            box = [round(i, 2) for i in box.tolist()]
+            x, y, x2, y2 = tuple(box)
+            draw.rectangle((x, y, x2, y2), outline="red", width=1)
+            draw.text((x, y), model.config.id2label[label.item()], fill="white")
+        # Save the image
+        image.save("output.jpg")
+    
 
 if __name__ == '__main__':
-    main()
+    main()  
