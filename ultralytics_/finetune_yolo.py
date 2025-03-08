754fe5b665bca73d7d1ea7a03b024ca9f3b1f122
Pol Rosines <pol.rosines@basetis.com>
2025-03-08 20:37:06 +0100

    reduced batch size

--- ultralytics_/finetune_yolo.py
+++ ultralytics_/finetune_yolo.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         data=config_dataset_path,
         epochs=300,
         patience=15,
-        batch=-1,
+        batch=64,
         save_period=50,
         cache=True,
         device='0',
