ad24a183aa9ddef588ab4059dc2f571c92bf86df
Pol Rosines <pol.rosines@basetis.com>
2025-03-08 18:59:32 +0100

    stream change

--- ultralytics_/inference.py
+++ ultralytics_/inference.py
@@ -8,7 +8,7 @@ def get_image_paths(sequence_path):
 
 
 def main(model_name, sequence_path, save_path, inference_arguments):
-    model = YOLO(model_name, stream = True) # load_model
+    model = YOLO(model_name) # load_model
     image_paths = sorted(get_image_paths(sequence_path))
     results = model.predict(image_paths, **inference_arguments)
     sequence_name = os.path.basename(sequence_path)
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     sequence_path = "/ghome/c5mcv03/KITTI-MOTS-YOLO/validation/images"
     
     output_inferred_images_path = os.path.join(consts.BASE_PATH_OUTPUT, "inference_images")
-    inference_arguments = {'classes': consts.YOLO_CLASSES}
+    inference_arguments = {'classes': consts.YOLO_CLASSES, "stream": True}
     main(model_path, sequence_path, output_inferred_images_path, inference_arguments)
