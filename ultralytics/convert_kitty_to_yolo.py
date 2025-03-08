5b8640afdc7d4298e08ce8a1f2125fd32854b592
Pol Rosines <pol.rosines@basetis.com>
2025-03-08 13:10:20 +0100

    inference

--- ultralytics/convert_kitty_to_yolo.py
+++ ultralytics/convert_kitty_to_yolo.py
@@ -1,7 +1,5 @@
 import os
-import numpy as np
 from pycocotools.mask import toBbox
-import matplotlib.pyplot as plt
 import shutil
 
 KITTI2COCO = {
@@ -13,7 +11,7 @@ def extract_yolo_annotations(ann_filepath):
     print(f"Processing: {ann_filepath}")
     with open(ann_filepath) as file:
         objs = file.readlines()
-    
+
     obj_timeframes = []
     img_height, img_width = 0, 0  # Placeholder values to be updated dynamically
 
@@ -25,6 +23,7 @@ def extract_yolo_annotations(ann_filepath):
         obj_class_id = int(obj_info[2])
         if obj_class_id == 10:
             continue
+        assert obj_class_id in KITTI2COCO, f"Unknown class ID: {obj_class_id}"
         obj_timeframes.append(obj_info[0])
         img_height = int(obj_info[3])  # Assuming the height is stored at index 3
         img_width = int(obj_info[4])   # Assuming the width is stored at index 4
@@ -37,7 +36,6 @@ def extract_yolo_annotations(ann_filepath):
         }
         rle_list.append(rle)
         obj_info_list.append((KITTI2COCO[obj_class_id], img_width, img_height))
-    print()
     try:
         # decoded_masks = decode_rle_batch(rle_list)
         decoded_bboxes = toBbox(rle_list)
@@ -49,6 +47,7 @@ def extract_yolo_annotations(ann_filepath):
 
     # Iterate over each bounding box to compute YOLO format values
     for i, bbox in enumerate(decoded_bboxes):
+        assert len(decoded_bboxes) == len(obj_timeframes) == len(obj_info_list)
         obj_timeframe = obj_timeframes[i].zfill(6)
         if obj_timeframe not in yolo_annotations:
             yolo_annotations[obj_timeframe] = []
@@ -62,47 +61,65 @@ def extract_yolo_annotations(ann_filepath):
         h_norm = h / img_height
         
         yolo_annotation = f"{obj_class_id} {x_center} {y_center} {w_norm} {h_norm}"
-        print(obj_timeframe, end=' ')
         yolo_annotations[obj_timeframe].append(yolo_annotation)
     return yolo_annotations
 
-def convert_kitti_to_yolo(kitti_anno_dir, output_dir, copy_images=False):
-    os.makedirs(output_dir, exist_ok=True)
-    output_dir_images = os.path.join(output_dir, "images")
-    os.makedirs(output_dir_images, exist_ok=True)
-    output_dir_labels = os.path.join(output_dir, "labels")
-    os.makedirs(output_dir_labels, exist_ok=True)
-    for ann_filename in sorted(os.listdir(kitti_anno_dir)):
-        ann_filepath = os.path.join(kitti_anno_dir, ann_filename)
-        yolo_annotations = extract_yolo_annotations(ann_filepath)
+def convert_kitti_to_yolo(kitti_anno_dir, output_dir, training_sequences, validation_sequences, copy_images=False):
+    def process_kitti_annotations(kitti_anno_dir, output_dir, sequence_ids, copy_images, folder_name):
+        output_dir = os.path.join(output_dir, folder_name)
+        os.makedirs(output_dir, exist_ok=True)
+        output_dir_images = os.path.join(output_dir, "images")
+        os.makedirs(output_dir_images, exist_ok=True)
+        output_dir_labels = os.path.join(output_dir, "labels")
+        os.makedirs(output_dir_labels, exist_ok=True)
         
-        # Save YOLO annotations to file
-        if yolo_annotations:
-            for yolo_img_label, yolo_img_objects in yolo_annotations.items():
-                sequence = os.path.basename(ann_filename).replace('.txt', '')
-                yolo_ann_filename = f"{sequence}_{yolo_img_label}.txt"
-                yolo_ann_filepath = os.path.join(output_dir_labels, yolo_ann_filename)
-                with open(yolo_ann_filepath, 'w') as f:
-                    f.write('\n'.join(yolo_img_objects))
-                print(f"Saved YOLO annotations to {yolo_ann_filepath}")
+        for ann_filename in sorted(os.listdir(kitti_anno_dir)):
+            if ann_filename.replace('.txt', '') not in sequence_ids:
+                continue
+            ann_filepath = os.path.join(kitti_anno_dir, ann_filename)
+            yolo_annotations = extract_yolo_annotations(ann_filepath)
 
-        if copy_images:
-            # Copy images to the output directory
-            img_dir = os.path.join(kitti_anno_dir, "..", "training", "image_02")
-            for root, _, files in os.walk(img_dir):
-                for file in files:
-                    if file.endswith(".png") or file.endswith(".jpg"):
-                        folder_name = os.path.basename(root)
-                        new_file_name = f"{folder_name}_{file}"
-                        src_file_path = os.path.join(root, file)
-                        dst_file_path = os.path.join(output_dir_images, new_file_name)
-                        os.makedirs(os.path.dirname(dst_file_path), exist_ok=True)
-                        shutil.copy(src_file_path, dst_file_path)
-                        print(f"Copied {src_file_path} to {dst_file_path}")
-        
+            # Save YOLO annotations to file
+            if yolo_annotations:
+                for yolo_img_label, yolo_img_objects in yolo_annotations.items():
+                    sequence = os.path.basename(ann_filename).replace('.txt', '')
+                    yolo_ann_filename = f"{sequence}_{yolo_img_label}.txt"
+                    yolo_ann_filepath = os.path.join(output_dir_labels, yolo_ann_filename)
+                    with open(yolo_ann_filepath, 'w') as f:
+                        f.write('\n'.join(yolo_img_objects))
+                    print(f"Saved YOLO annotations to {yolo_ann_filepath}")
+
+            if copy_images:
+                # Copy images to the output directory
+                img_dir = os.path.join(kitti_anno_dir, "..", "training", "image_02")
+                for root, _, files in os.walk(img_dir):
+                    for file in files:
+                        if file.endswith(".png") or file.endswith(".jpg"):
+                            folder_name = os.path.basename(root)
+                            new_file_name = f"{folder_name}_{file}"
+                            src_file_path = os.path.join(root, file)
+                            dst_file_path = os.path.join(output_dir_images, new_file_name)
+                            os.makedirs(os.path.dirname(dst_file_path), exist_ok=True)
+                            shutil.copy(src_file_path, dst_file_path)
+                            # print(f"Copied {src_file_path} to {dst_file_path}")
+
+    # Call the function for training and validation sequences
+    process_kitti_annotations(kitti_anno_dir, output_dir, training_sequences, copy_images, "training")
+    process_kitti_annotations(kitti_anno_dir, output_dir, validation_sequences, copy_images, "validation")
+    # process_kitti_annotations(kitti_anno_dir, output_dir, training_sequences + validation_sequences, copy_images, "all")
+
+
 if __name__ == "__main__":
-    # kitti_dir_input = "/ghome/c5mcv03/mcv/datasets/C5/KITTI-MOTS/instances_txt"
-    kitti_dir_input = "/projects/master/c5/KITTI_MOTS/instances_txt"
-    # kitti_dir_output = "/ghome/c5mcv03/test/KITTI-MOTS-YOLO"
-    kitti_dir_output = "/projects/master/c5/test"
-    convert_kitti_to_yolo(kitti_dir_input, kitti_dir_output, copy_images=True)
+    kitti_dir_input = "/ghome/c5mcv03/mcv/datasets/C5/KITTI-MOTS/instances_txt"
+    # kitti_dir_input = "/projects/master/c5/KITTI_MOTS/instances_txt"
+    kitti_dir_output = "/ghome/c5mcv03/test/KITTI-MOTS-YOLO"
+    # kitti_dir_output = "/projects/master/c5/test/KITTI_MOTS_YOLO"
+
+    training_sequences = [str(sequence).zfill(4) for sequence in list(range(0, 16))]
+    validation_sequences = [str(sequence).zfill(4) for sequence in list(range(16, 21))]
+    
+    convert_kitti_to_yolo(kitti_anno_dir = kitti_dir_input,
+                          output_dir = kitti_dir_output,
+                          training_sequences = training_sequences,
+                          validation_sequences = validation_sequences,
+                          copy_images = True)
