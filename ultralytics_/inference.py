832d9cac086e6762edf84e7cbd01b512ab4c7883
Pol Rosines <pol.rosines@basetis.com>
2025-03-08 18:47:13 +0100

    minot config changes

--- ultralytics_/inference.py
+++ ultralytics_/inference.py
@@ -8,7 +8,7 @@ def get_image_paths(sequence_path):
 
 
 def main(model_name, sequence_path, save_path, inference_arguments):
-    model = YOLO(model_name) # load_model
+    model = YOLO(model_name, stream = True) # load_model
     image_paths = sorted(get_image_paths(sequence_path))
     results = model.predict(image_paths, **inference_arguments)
     sequence_name = os.path.basename(sequence_path)
@@ -16,8 +16,8 @@ def main(model_name, sequence_path, save_path, inference_arguments):
         r.save(os.path.join(save_path, f"result_{sequence_name}_{i}.png"))
 
 if __name__ == "__main__":
-    model_path = "yolo11n.pt"
-    sequence_path = "/projects/master/c5/KITTI_MOTS/training/image_02/0000"
+    model_path = "/ghome/c5mcv03/mcv-c5-group-3/outputs/pol/models/yolo11n.pt"
+    sequence_path = "/ghome/c5mcv03/KITTI-MOTS-YOLO/validation/images"
     
     output_inferred_images_path = os.path.join(consts.BASE_PATH_OUTPUT, "inference_images")
     inference_arguments = {'classes': consts.YOLO_CLASSES}
