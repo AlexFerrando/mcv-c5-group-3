302a45c43eddd3b7abfe655f1333e6a7506778b7
Pol Rosines <pol.rosines@basetis.com>
2025-03-08 19:55:56 +0100

    finetuning yolo v1

---
+++ ultralytics_/finetune.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+
+from ultralytics import YOLO
+import wandb
+from wandb.integration.ultralytics import add_wandb_callback
+
+import consts
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate YOLO model")
+    parser.add_argument("--model_path", type=str, help="Path to the YOLO model", default="yolo11n.pt")
+    parser.add_argument("--output_path", type=str, help="Path to save the results", default="/ghome/c5mcv03/mcv-c5-group-3/outputs/pol/job_outputs")
+    args = parser.parse_args()
+
+    wandb.login(key = "8410a2da3f323633210ca8d25ce6862368d9f489")
+    model = YOLO(args.model_path)
+    config_dataset_path = os.path.join(consts.PATH_KITTI_MOTS_YOLO, "kitti_mots_config.yaml")
+    add_wandb_callback(model, enable_model_checkpointing = True)
+    model.train(
+        data=config_dataset_path,
+        epochs=300,
+        patience=15,
+        batch=-1,
+        save_period=50,
+        cache=True,
+        device='0',
+        project=args.output_path,
+        name=f"{args.model_path}_trained"
+    )
