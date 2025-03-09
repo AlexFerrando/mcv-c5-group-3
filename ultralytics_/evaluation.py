f1440279b2ae4b114ea2cba00a90cb2895d2c14a
Pol Rosines <pol.rosines@basetis.com>
2025-03-09 12:13:48 +0100

    added run name to evaluation yolo

--- ultralytics_/evaluation.py
+++ ultralytics_/evaluation.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str, help="Path to the YOLO model")
     parser.add_argument("--output_path", type=str, help="Path to save the results", default="/ghome/c5mcv03/mcv-c5-group-3/outputs/pol/job_outputs")
     parser.add_argument("--split_val", type=bool, help="Evaluate the validation set", default=True)
+    parser.add_argument("--val_name", type=str, help="Name for the validation results", default="off the shelf hybrid outputs pretrained model yolo11n")
     args = parser.parse_args()
 
     wandb.login(key = "8410a2da3f323633210ca8d25ce6862368d9f489")
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     results = model.val(data=os.path.join(consts.PATH_KITTI_MOTS_YOLO, "kitti_mots_config.yaml"),
                         classes=consts.YOLO_CLASSES, cache=False, project=args.output_path,
                         split="val" if args.split_val else "train",
-                        name = "off the shelf hybrid outputs pretrained model yolo11n")
+                        name=args.val_name)
     print(results.box.map)  # mAP50-95
     print(results.box.map50)  # mAP50
     print(results.box.map75)  # mAP75
