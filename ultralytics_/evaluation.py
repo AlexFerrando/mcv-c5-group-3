095d6ebbc2d31f16afaf79cae1216d8b1bc40434
Pol Rosines <pol.rosines@basetis.com>
2025-03-09 01:26:25 +0100

    added name to the evaluation wandb run

--- ultralytics_/evaluation.py
+++ ultralytics_/evaluation.py
@@ -18,4 +18,5 @@ if __name__ == "__main__":
     model = YOLO(args.model_path)
     results = model.val(data=os.path.join(consts.PATH_KITTI_MOTS_YOLO, "kitti_mots_config.yaml"),
                         classes=consts.YOLO_CLASSES, cache=False, project=args.output_path,
-                        save_hybrid=True, split="val" if args.split_val else "train")
+                        save_hybrid=True, split="val" if args.split_val else "train",
+                        name = "off the shelf hybrid outputs pretrained model yolo11n")
