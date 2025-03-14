fcc52b79b7a2c0dd48c4806930fea05b68ba0e71
Uriiol1808 <oriolmarin18@gmail.com>
2025-03-14 21:37:00 +0100

    `-´

---
+++ detectron2/week2/consts.py
@@ -0,0 +1,6 @@
+# Path to data
+KITTI_MOTS_PATH = '/ghome/c5mcv03/mcv/datasets/C5/KITTI-MOTS/'
+# KITTI_MOTS_PATH = '/ghome/c5mcv03/KITTI_MOTS/'
+KITTI_MOTS_FAST = '/ghome/c5mcv03/KITTI-MOTS-FAST/'
+# Model info
+MODEL_NAME = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
