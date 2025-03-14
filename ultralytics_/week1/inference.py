b07821be76093c63019c780b549a9b0d8a6552fc
Pol Rosines <pol.rosines@basetis.com>
2025-03-14 20:06:55 +0100

    init w2 yolo

--- ultralytics_/week1/inference.py
+++ ultralytics_/week1/inference.py
@@ -10,15 +10,17 @@ def get_image_paths(sequence_path):
 def main(model_name, sequence_path, save_path, inference_arguments):
     model = YOLO(model_name) # load_model
     image_paths = sorted(get_image_paths(sequence_path))
-    results = model.predict(image_paths, **inference_arguments)
+    print(image_paths)
+    results = model.predict(image_paths[-350:], **inference_arguments)
     sequence_name = os.path.basename(sequence_path)
     for i, r in enumerate(results):
-        print("saving image to", os.path.join(save_path, f"result_{sequence_name}_{i}.png"))
-        r.save(os.path.join(save_path, f"result_{sequence_name}_{i}.png"))
+        print("saving image to", os.path.join(save_path, f"result_{sequence_name}_{int(i) + len(image_paths) - 350}.png"))
+        r.save(os.path.join(save_path, f"result_{sequence_name}_{int(i) + len(image_paths) - 350}.png"))
 
 if __name__ == "__main__":
-    model_path = "/ghome/c5mcv03/mcv-c5-group-3/outputs/pol/models/yolo11n.pt"
     model_path = "yolo11n.pt"
+    model_path = "/ghome/c5mcv03/mcv-c5-group-3/outputs/pol/models/yolo11n.pt"
+    model_path = "/projects/master/c5/best.pt"
     
     sequence_path = "/ghome/c5mcv03/KITTI-MOTS-YOLO/validation/images"
     sequence_path = "/projects/master/c5/KITTI-MOTS-big/training/image_02/0019"
