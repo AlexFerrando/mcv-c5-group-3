1c061c22d0bd33560541de13ad9ceb01f6621234
Uriiol1808 <oriolmarin18@gmail.com>
2025-03-15 12:32:01 +0100

    update detectron

--- detectron2/week2/consts.py
+++ detectron2/week2/consts.py
@@ -3,4 +3,4 @@ KITTI_MOTS_PATH = '/ghome/c5mcv03/mcv/datasets/C5/KITTI-MOTS/'
 # KITTI_MOTS_PATH = '/ghome/c5mcv03/KITTI_MOTS/'
 KITTI_MOTS_FAST = '/ghome/c5mcv03/KITTI-MOTS-FAST/'
 # Model info
-MODEL_NAME = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+MODEL_NAME = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
