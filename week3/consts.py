9f370e7143c444be6c00d31c627a41e745ac2a73
Uriiol1808 <oriolmarin18@gmail.com>
2025-03-22 11:39:22 +0100

    update consts

--- week3/consts.py
+++ week3/consts.py
@@ -2,5 +2,6 @@ DATA_PATH = '/ghome/c5mcv03/mcv-c5-group-3/week3/archive'
 DATA_PATH_ALEX = "/home/alex/Documents/MCV/C5/mcv-c5-group-3/week3/archive"
 DATA_PATH_ARNAU = '/Users/arnaubarrera/Desktop/MSc Computer Vision/C5. Visual Recognition/mcv-c5-group-3/week3/archive'
 DATA_PATH_POL = '/projects/master/c5/mcv-c5-group-3/archive/food-ingredients-and-recipe-dataset-with-images'
+DATA_PATH_URI = '/Users/Usuario/Documents/MCV/C5/archive'
 
 SEED = 42
