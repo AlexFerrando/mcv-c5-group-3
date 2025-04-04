77fe852663fbbc72982299c43d548b3905d8a994
Pol Rosines <polrosines@gmail.com>
2025-04-04 19:45:09 +0200

    updated download models

--- week4/download_model.py
+++ week4/download_model.py
@@ -5,10 +5,16 @@ wandb.login(key='8410a2da3f323633210ca8d25ce6862368d9f489')
 # Initialize a W&B run (ensure you’re logged in or have set your API key)
 run = wandb.init(project="C5-W4", entity="arnalytics-universitat-aut-noma-de-barcelona")
 
-# Access the artifact by specifying its name and optionally its version (e.g., 'model:latest')
-artifact = run.use_artifact('arnalytics-universitat-aut-noma-de-barcelona/C5-W4/best_model:v0')
 
-# Download the artifact locally; this returns the path to the downloaded files
-artifact_dir = artifact.download()
+paths = [
+    'arnalytics-universitat-aut-noma-de-barcelona/C5-W4/fine-tune-encoder:v0',
+    'arnalytics-universitat-aut-noma-de-barcelona/C5-W4/fine-tune-both:v0'
+]
+for path in paths:
+    # Access the artifact by specifying its name and optionally its version (e.g., 'model:latest')
+    artifact = run.use_artifact(path)
 
-print(f"Artifact downloaded to: {artifact_dir}")
+    # Download the artifact locally; this returns the path to the downloaded files
+    artifact_dir = artifact.download()
+
+    print(f"Artifact downloaded to: {artifact_dir}")
