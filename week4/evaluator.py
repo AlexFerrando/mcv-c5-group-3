467d9681d0f819add3b40ca4031cb98840ff6624
Pol Rosines <polrosines@gmail.com>
2025-04-05 17:06:52 +0200

    try except in evaluators

--- week4/evaluator.py
+++ week4/evaluator.py
@@ -27,9 +27,28 @@ class Evaluator():
             nltk.download('omw-1.4', quiet=True)
 
     def evaluate(self, ground_truth: List[str], prediction: List[str]):
-        res_b1 = self.bleu.compute(predictions=prediction, references=ground_truth, max_order=1)
-        res_b2 = self.bleu.compute(predictions=prediction, references=ground_truth, max_order=2)
-        res_r = self.rouge.compute(predictions=prediction, references=ground_truth)
-        res_m = self.meteor.compute(predictions=prediction, references=ground_truth)
+        try:
+            res_b1 = self.bleu.compute(predictions=prediction, references=ground_truth, max_order=1)
+        except Exception as e:
+            print(f"Error computing BLEU-1: {e}")
+            res_b1 = 0
+        
+        try:
+            res_b2 = self.bleu.compute(predictions=prediction, references=ground_truth, max_order=2)
+        except Exception as e:
+            print(f"Error computing BLEU-2: {e}")
+            res_b2 = 0
+        
+        try:
+            res_r = self.rouge.compute(predictions=prediction, references=ground_truth)
+        except Exception as e:
+            print(f"Error computing ROUGE: {e}")
+            res_r = 0
+            
+        try:
+            res_m = self.meteor.compute(predictions=prediction, references=ground_truth)
+        except Exception as e:
+            print(f"Error computing METEOR: {e}")
+            res_m = 0
 
         return {'bleu1': res_b1, 'bleu2': res_b2, 'rouge': res_r, 'meteor': res_m}
